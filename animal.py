c664162319bc9cab17d4d0dee63c8257315eb659
ramseybelle <108037203+ramseybelle@users.noreply.github.com>
2022-06-28 22:39:12 -0700

    This was difficult remember to go to tool bar aboove on mac no the green check...that did absolutley nothing for you

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Mcnabb Ramsey
+# GitHub username: Rat
+# Date: 6/25/2022
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+fave_animal = input("Please enter your favorite animal:")
+print("Your favorite animal is the " + fave_animal + ".")
